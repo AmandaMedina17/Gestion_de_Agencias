09c9e14ae1b50470fd0b8f5c4071b67f56faa309
AmandaMedina17 <155869016+AmandaMedina17@users.noreply.github.com>
2025-11-05 11:46:37 -0500

    feat:frontend

--- src/DomainLayer/Repositories/IApprenticeRepository.ts
+++
@@ -1,9 +0,0 @@
-import { ApprenticeID } from "../Value Objects/IDs";
-import { ApprenticeEntity } from "../Entities/Apprentice";
-import { EvaluationEntity } from "../Entities/Evaluation";
-import { IRepository } from "./IRepository";
-
-export interface IApprenticeRepository
-  extends IRepository<ApprenticeEntity, ApprenticeID> {
-  getApprenticeEvaluations(id: ApprenticeID): Promise<EvaluationEntity[]>;
-}
--- src/InfraestructureLayer/database/IMapper.ts
+++
@@ -1,4 +0,0 @@
-export interface IMapper <DomainEntity, DataBaseEntity> {
-    toDomainEntity(dataBaseEntity : DataBaseEntity) : DomainEntity;
-    toDataBaseEntity(domainEntity : DomainEntity) : DataBaseEntity;
-}
