cc61c0dd938ce507c6f4786dd94c3ba593ebad5a
Juan Miguel <juanmiguelmaestre4@gmail.com>
2025-12-02 05:49:49 -0500

    build : fixing some dependencies problems on in package.json

--- backend/src/ApplicationLayer/DTOs/billboardDto/update.billboard.dto.ts
+++ backend/src/ApplicationLayer/DTOs/billboardDto/update.billboard.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/mapped-types";
+import { PartialType } from "@nestjs/swagger";
 import { CreateBillBoardListDto } from "./create.billboard.dto";
 
 export class UpdateBillboardListDto extends PartialType(CreateBillBoardListDto){}
